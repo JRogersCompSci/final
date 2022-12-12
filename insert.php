6c0cee4e8ff8ba40dc6d7fa8ff52c9a91b8a8937
JRogersCompSci <120347373+JRogersCompSci@users.noreply.github.com>
2022-12-11 19:28:56 -0600

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,17 @@
+<?<php>
+$email = $_POST['email']
+$pass = $_POST['pass']
+if| !empty($email) || !empty($pass) ||{
+{
+    $host = "localhost";
+    $dbUsername = "root"
+    
+
+}
+}{#code}
+else
+{
+    echo "All feilds are required";
+    die();
+}
+<?>
---
+++ java.js
@@ -0,0 +1,25 @@
+function ValidateEmail(mail) 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
+  {
+    return (true)
+  }
+    alert("You have entered an invalid email address!")
+    return (false)
+}
+function ValidateEmail(inputText)
+{
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(inputText.value.match(mailformat))
+{
+alert("Valid email address!");
+document.form1.text1.focus();
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+document.form1.text1.focus();
+return false;
+}
+}
